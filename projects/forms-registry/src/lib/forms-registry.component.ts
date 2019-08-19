3e64be77db0a07f47845215f119f70758a9f225d
Giora Guttsait <giora111@gmail.com>
2019-08-20 00:55:44 +0300

    ng g lib forms-registry

---
+++ projects/forms-registry/src/lib/forms-registry.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-forms-registry',
+  template: `
+    <p>
+      forms-registry works!
+    </p>
+  `,
+  styles: []
+})
+export class FormsRegistryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ projects/forms-registry/src/lib/forms-registry.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { FormsRegistryComponent } from './forms-registry.component';
+
+
+
+@NgModule({
+  declarations: [FormsRegistryComponent],
+  imports: [
+  ],
+  exports: [FormsRegistryComponent]
+})
+export class FormsRegistryModule { }
