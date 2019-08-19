77efe10418014a82be2d206a80b0120806b5ab7d
Giora Guttsait <giora111@gmail.com>
2019-08-19 23:27:36 +0300

    ng g lib submodules

---
+++ projects/submodules/src/lib/submodules.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { SubmodulesComponent } from './submodules.component';
+
+
+
+@NgModule({
+  declarations: [SubmodulesComponent],
+  imports: [
+  ],
+  exports: [SubmodulesComponent]
+})
+export class SubmodulesModule { }
---
+++ projects/submodules/src/public-api.ts
@@ -0,0 +1,7 @@
+/*
+ * Public API Surface of submodules
+ */
+
+export * from './lib/submodules.service';
+export * from './lib/submodules.component';
+export * from './lib/submodules.module';
