6092b5f6cc6227385f2d14d141cc19aef3d65607
Giora Guttsait <giora111@gmail.com>
2019-08-20 01:50:32 +0300

    Get rid of old lazy-feature lib

--- projects/lazy-feature/src/lib/interfaces.ts
+++
@@ -1,5 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface CanActivate {
-  canActivate(): boolean | Promise<boolean> | Observable<boolean>;
-}
--- projects/lazy-feature/src/lib/utils/type_guard.ts
+++
@@ -1,9 +0,0 @@
-import { CanActivate } from '../interfaces';
-
-export function isFunction<T>(v: any): v is T {
-  return typeof v === 'function';
-}
-
-export function isCanActivate(guard: any): guard is CanActivate {
-  return guard && isFunction<CanActivate>(guard.canActivate);
-}
