5f3712c7d6005f45e2734ace3625e9cfdd9b8435
Giora Guttsait <giora111@gmail.com>
2019-08-21 00:30:04 +0300

    Prevent lazy-loaded submodules from being loaded multiple times

--- projects/submodules/src/lib/submodule-loader.service.ts
+++ projects/submodules/src/lib/submodule-loader.service.ts
@@ -1,7 +1,7 @@
 import { Compiler, Injectable, Injector, NgModuleFactory } from '@angular/core';
-import { from, Observable, of } from 'rxjs';
-import { map, mergeMap } from 'rxjs/operators';
-import { LoadedLazySubmodule, LAZY_SUBMODULES } from './config';
+import { EMPTY, from, Observable, of } from 'rxjs';
+import { finalize, map, mergeMap } from 'rxjs/operators';
+import { LAZY_SUBMODULES, LoadedLazySubmodule } from './config';
 import { LazySubmodule } from './interfaces';
 import { checkGuards } from './operators';
 import { wrapIntoObservable } from './utils/collections';
@@ -10,14 +10,20 @@ import { wrapIntoObservable } from './utils/collections';
   providedIn: 'root'
 })
 export class SubmoduleLoaderService {
+  private loadedSubmodules = false;
   constructor(
     private injector: Injector,
     private compiler: Compiler,
   ) { }
 
   lazyLoadSubmodules(): Observable<LoadedLazySubmodule> {
+    if (this.loadedSubmodules) {
+      return EMPTY;
+    }
     const lazySubmodules = this.injector.get(LAZY_SUBMODULES);
-    return from(lazySubmodules).pipe(mergeMap(m => this.loadModule(m)));
+    return from(lazySubmodules).pipe(
+      mergeMap(m => this.loadModule(m)),
+      finalize(() => this.loadedSubmodules = true));
   }
 
   loadModule(submodule: LazySubmodule): Observable<LoadedLazySubmodule> {
