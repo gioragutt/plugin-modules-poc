b01e27e789274a9516e76daa91234db69ac5fa03
Giora Guttsait <giora111@gmail.com>
2019-08-21 18:18:01 +0300

    Add way to diffrentiate between Feature3 forms

--- src/app/feature3/feature3-page/feature3-page.component.ts
+++ src/app/feature3/feature3-page/feature3-page.component.ts
@@ -5,11 +5,7 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './feature3-page.component.html',
   styleUrls: ['./feature3-page.component.scss']
 })
-export class Feature3PageComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class Feature3PageComponent {
+  static counter = 0;
+  id = Feature3PageComponent.counter++;
 }
